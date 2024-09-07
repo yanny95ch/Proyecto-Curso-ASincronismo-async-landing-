e8acb7b237a3e2ff9cf3bf70b36d841485bbe996
yannych1995 <yannymaly@gmal.com>
2024-09-07 17:36:29 -0500

    Proyecto

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,4 +1,5 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=%40TVfordogs-2106&part=snippet%2Cid&order=date&maxResults=9';
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCZszCX_TfylksyABpzXiRtg&part=snippet%2Cid&order=date&maxResults=50';
+//const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCBVjMGOIkavEAhyqpxJ73Dw&part=snippet%2Cid&order=date&maxResults=50';
 
 const content = null || document.getElementById('content');
 const options = {
@@ -11,9 +12,10 @@ const options = {
 
 
 async function fetchData(urlApi) {
-    const response = await fetch(urlApi,options);
-    const data= await response.json();
-    return data;
+  const response = await fetch(urlApi,options);
+  console.log('response:', response)
+  const data= await response.json();
+  return data;
 }
 (async()=>{
     try {
@@ -23,7 +25,7 @@ async function fetchData(urlApi) {
         <div class="group relative">
           <div
             class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-            <img src="${video.snippet.thumbnail.high.url}"alt="${video.snippet.description}" class="w-full">
+            <img src="${video.snippet.thumbnails.high.url}"alt="${video.snippet.description}" class="w-full">
         </div>
         <div class="mt-4 flex justify-between">
             <h3 class="text-sm text-gray-700">
@@ -41,15 +43,3 @@ async function fetchData(urlApi) {
         
     }
 })();
-
-
-
-/*
-try {
-	const response = await fetch(urlApi, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-*/
